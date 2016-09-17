cb1abbff50b3304eb5ccf24d714c42a27b740a48
Thomas Droxler <thomas.droxler@gmail.com>
2016-09-17 14:34:08 +0200

    Introduce `Codec.Sarsi.ELM`

---
+++ src/Codec/Sarsi/ELM.hs
@@ -0,0 +1,21 @@
+module Codec.Sarsi.ELM where
+
+import Codec.Sarsi
+import Data.Text (pack)
+import Data.Maybe (fromMaybe)
+
+import qualified Codec.ELM.Log as Log
+import qualified Data.Vector as Vector
+import qualified Data.Text as Text
+
+fromELMLog :: Log.Message-> Message
+fromELMLog (Log.Message tag overview details region subRegion file level) =
+  let col = Log.column . Log.start $ fromMaybe region subRegion
+      row = Log.line . Log.start $ fromMaybe region subRegion
+  in Message
+       (Location (pack file) col row)
+       (fromELMLevel level)
+       (Vector.fromList [(Text.concat [pack tag, pack ": ", pack overview, pack " ", pack details])])
+
+fromELMLevel :: String -> Level
+fromELMLevel txt = if txt == "warning" then Warning else Error
