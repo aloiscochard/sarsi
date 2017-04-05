e8a4f7635f31732b3fd8875804c041cb8a5b697f
Alois Cochard <alois.cochard@gmail.com>
2017-04-05 16:13:44 +0200

    Add support for an alternative format in `Codec.Sarsi.Rust`.

--- src/Codec/Sarsi/Rust.hs
+++ src/Codec/Sarsi/Rust.hs
@@ -6,19 +6,31 @@ import Codec.Sarsi (Level(..), Location(..), Message(..))
 import Data.Attoparsec.Text
 
 messageParser :: Parser Message
-messageParser = do
-  fp  <- takeWhile1 (\c -> c /= sepChar && c /= '\n' && c /= '\r') <* char sepChar
-  n   <- decimal <* char sepChar
-  c   <- decimal <* char sepChar
-  _   <- untilSep <* char sepChar
-  _   <- untilSpace *> space
-  l   <- choice
-    [ string "warning: " *> return Warning
-    , string "error: " *> return Error ]
-  txt <- untilLineBreak <* ("\n" <* end)
-  return $ Message (Location fp c n) l [txt]
-  where
+messageParser = choice [formatA, formatB] where
+    formatA    = do
+      fp  <- takeWhile1 (\c -> c /= sepChar && c /= '\n' && c /= '\r') <* char sepChar
+      n   <- decimal <* char sepChar
+      c   <- decimal <* char sepChar
+      _   <- untilSep <* char sepChar
+      _   <- untilSpace *> space
+      l   <- choice
+        [ string "warning: " *> return Warning
+        , string "error: " *> return Error ]
+      txt <- untilLineBreak <* ("\n" <* end)
+      return $ Message (Location fp c n) l [txt]
+
+    formatB    = do
+      l   <- choice
+        [ string "warning"  *> untilSep0 *> string ": " *> return Warning
+        , string "error"    *> untilSep0 *> string ": " *> return Error   ]
+      txt <- untilLineBreak <* "\n"
+      fp  <- "   --> " *> untilSep <* char sepChar
+      n   <- decimal <* char sepChar
+      c   <- decimal <* end
+      return $ Message (Location fp c n) l [txt]
+
     untilSep   = takeWhile1 $ \w -> w /= sepChar
+    untilSep0  = Data.Attoparsec.Text.takeWhile $ \w -> w /= ':'
     untilSpace = takeWhile1 $ \w -> w /= ' '
     end = choice [const () <$> "\n", endOfInput, return ()]
     sepChar = ':'
